c1f98c1bd48e7dae4ca0d8587f5de96db9e3db0a
KalvinRP <kalvinpratamarch@gmail.com>
2023-01-24 05:20:50 +0700

    minor bug fix

--- src/Components/synopsis.js
+++ src/Components/synopsis.js
@@ -153,7 +153,7 @@ export default function Synopsis() {
                         title='trailer'
                         width="100%"
                         height="360"
-                        src={`http://youtube.com/embed/${form?.ytid}`}
+                        src={`https://youtube.com/embed/${form?.ytid}`}
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen />
                     <h3 style={{ color: "#7E7E7E", marginTop: '3%' }}>{form?.genre}</h3>
